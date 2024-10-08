1f375ba7b99109692b21d61811cce7979d445827
olli20 <olena.voina@powercoders.org>
2024-10-08 10:33:42 +0200

    improve and separate the theme switcher into its own component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTheme } from '../../redux/slices/themeSlice';
+import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
 import css from './Navbar.module.css';
 
 const Navbar = () => {
-  const dispatch = useDispatch();
-  const currentTheme = useSelector((state) => state.theme.mode);
-
-  const [randomTheme, setRandomTheme] = useState('');
-
-  useEffect(() => {
-    if (currentTheme.startsWith('random')) {
-      document.body.setAttribute('data-theme', currentTheme); 
-    } else {
-      setRandomTheme(''); 
-      document.body.setAttribute('data-theme', currentTheme); 
-    }
-  }, [currentTheme]);
-
-  const handleThemeChange = (theme) => {
-    dispatch(setTheme(theme));
-  };
-
-  const handleRandomTheme = () => {
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    const selectedRandomTheme = `random${randomNumber}`;
-    setRandomTheme(selectedRandomTheme);
-    handleThemeChange(selectedRandomTheme);
-  };
-
   return (
     <nav className={css.navbar}>
       <Link to="/" className={css.logo}>Olena Voina</Link>
@@ -40,26 +14,7 @@ const Navbar = () => {
         <li><NavLink to="/contact" className={({ isActive }) => (isActive ? css.active : '')}>Contacts</NavLink></li>
       </ul>
 
-      <div className={css.themeSwitcher}>
-        <button
-          onClick={() => handleThemeChange('light')}
-          className={currentTheme === 'light' ? css.active : ''}
-        >
-          Light
-        </button>
-        <button
-          onClick={() => handleThemeChange('dark')}
-          className={currentTheme === 'dark' ? css.active : ''}
-        >
-          Dark
-        </button>
-        <button
-          onClick={handleRandomTheme}
-          className={randomTheme ? css.active : ''}
-        >
-          Random
-        </button>
-      </div>
+      <ThemeSwitcher />
     </nav>
   );
 };
---
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTheme } from '../../redux/slices/themeSlice';
+import { AiFillSun, AiFillMoon } from 'react-icons/ai';
+import css from './ThemeSwitcher.module.css'; 
+
+const ThemeSwitcher = () => {
+  const dispatch = useDispatch();
+  const currentTheme = useSelector((state) => state.theme.mode);
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', currentTheme); 
+  }, [currentTheme]);
+
+  const handleThemeChange = (theme) => {
+    dispatch(setTheme(theme));
+  };
+
+  return (
+    <div className={css.themeSwitcher}>
+      <button
+        onClick={() => handleThemeChange('light')}
+        className={currentTheme === 'light' ? css.active : ''}
+      >
+        <AiFillSun />
+      </button>
+      <button
+        onClick={() => handleThemeChange('dark')}
+        className={currentTheme === 'dark' ? css.active : ''}
+      >
+        <AiFillMoon />
+      </button>
+    </div>
+  );
+};
+
+export default ThemeSwitcher;
