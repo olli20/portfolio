3b4ef9903d4c263d22b7f69767825bf453ebf7bc
olli20 <olena.voina@powercoders.org>
2024-10-11 21:17:55 +0200

    add backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,46 @@
 import mongoose from 'mongoose';
-const DB_HOST = "mongodb+srv://admin:<pass>@cluster0.q4y5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import "dotenv/config";
 
-mongoose.set('strictQuery', true);
+import blogRouter from "./routes/blogRoutes.js";
 
-mongoose.connect(DB_HOST)
-    .then(() => console.log("succes"))
-    .catch(error =>  console.log(error));
+const app = express();
 
+const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.DB_HOST;
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const globalErrorHandler = (error, req, res, next) => {
+    const { status = 500, message = "Server error", data } = error;
+    res.status(status).json({ message, data });
+  };
+
+//ROUTES
+app.use("/blog", blogRouter);
+
+// handle not found error
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+// handle global error
+app.use(globalErrorHandler);
+
+//SERVER
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
