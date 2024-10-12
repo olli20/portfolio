1feca700a587f45aba074498aaa6a042451fb8e1
olli20 <olena.voina@powercoders.org>
2024-10-12 13:48:36 +0200

    remove backend folder

--- backend/controllers/errorController.js
+++
@@ -1,6 +0,0 @@
-const globalErrorHandler = (error, req, res, next) => {
-    const { status = 500, message = "Server error", data } = error;
-    res.status(status).json({ message, data });
-  };
-  
-export default globalErrorHandler;
