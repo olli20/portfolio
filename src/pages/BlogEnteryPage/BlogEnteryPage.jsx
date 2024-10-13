d73551f3c96ad9f71ccb47b244bd881b205bb058
olli20 <olena.voina@powercoders.org>
2024-10-13 19:27:03 +0200

    add navigation between blog posts

--- src/pages/BlogEnteryPage/BlogEnteryPage.jsx
+++ src/pages/BlogEnteryPage/BlogEnteryPage.jsx
@@ -1,53 +1,69 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
-
-import { Loader } from '../../components/Loader/Loader'
+import { Loader } from '../../components/Loader/Loader';
 import { getPostById } from '../../api/api';
-
+import { Button } from '../../components/Button/Button';
 import css from './BlogEnteryPage.module.css';
 
-const BlogEnteryPage = () => {
-  const {blogId} = useParams();
+const BlogEntryPage = () => {
+  const { blogId } = useParams();
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from || "/";
 
-  //state
+  // state
   const [data, setData] = useState(null);  
   const [loading, setLoading] = useState(true);  
   const [error, setError] = useState(null); 
 
-  //fetching data
+  // fetching data
   useEffect(() => {
     const fetchPost = async () => {
       try {
         const post = await getPostById(blogId);
         setData(post);
       } catch (error) {
-        console.log(error);
+        console.error(error);
         setError('Failed to load the blog entry.');
       } finally {
         setLoading(false);
       }
     };
-
     fetchPost();
   }, [blogId]);
 
-  const goBack = useCallback(() => navigate(from), [from, navigate]);
+  const goToPreviousPost = () => {
+    if (data?.previousPostId) {
+      navigate(`/blog/${data.previousPostId}`);
+    }
+  };
+
+  const goToNextPost = () => {
+    if (data?.nextPostId) {
+      navigate(`/blog/${data.nextPostId}`);
+    }
+  };
+
+  const goBack = useCallback(() => navigate('/blog'), [navigate]);
 
   if (loading) return <div><Loader /></div>;
   if (error) return <p>{error}</p>;
   if (!data) return <p>No post found</p>;
 
   return (
-      <div className={css.container}>
+    <div className={css.container}>
+      <nav className={css.navigationButtons}>
+        {data.previousPostId && <Button onClick={goToPreviousPost}>Previous Post</Button>}
+        <Button onClick={goBack}>Back to Blog</Button>
+        {data.nextPostId && <Button onClick={goToNextPost}>Next Post</Button>}
+      </nav>
+      <main>
         <h1>{data.title}</h1>
-        <a onClick={goBack}>Go Back</a>
-          <ReactMarkdown>{data.content}</ReactMarkdown>
-      </div>
+        <ReactMarkdown>{data.content}</ReactMarkdown>
+      </main>
+    </div>
   );
 };
 
-export default BlogEnteryPage;
+export default BlogEntryPage;
