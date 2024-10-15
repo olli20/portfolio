cdb6b02044b8a34eea63d8633e18d95707ea4539
olli20 <olena.voina@powercoders.org>
2024-10-15 11:02:00 +0200

    fix all tags selection

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -4,7 +4,7 @@ import {Routes, Route} from "react-router-dom";
 import {Loader} from "./components/Loader/Loader";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const PortfolioPage = lazy(() => import("./pages/PortfolioPage/PortfolioPage"));
+// const PortfolioPage = lazy(() => import("./pages/PortfolioPage/PortfolioPage"));
 const BlogPage = lazy(() => import("./pages/BlogPage/BlogPage"));
 const BlogEnteryPage = lazy(() => import("./pages/BlogEnteryPage/BlogEnteryPage"));
 const ContactPage = lazy(() => import("./pages/ContactPage/ContactPage"));
@@ -15,7 +15,6 @@ export const UserRoutes = () => {
         <Suspense fallback={<Loader />}>
             <Routes basename="/portfolio">
                 <Route path="/" element={<HomePage />} />
-                <Route path="/portfolio" element={<PortfolioPage />} />
                 <Route path="/blog" element={<BlogPage />} />
                 <Route path="/blog/:blogId" element={<BlogEnteryPage />} />
                 <Route path="/contact" element={<ContactPage />} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher';
 import css from './Navbar.module.css';
@@ -9,7 +8,7 @@ export const Navbar = () => {
       <Link to="/" className={css.logo}>Olena Voina</Link>
       <ul className={css.navbarMenu}>
         <li><NavLink to="/" className={({ isActive }) => (isActive ? css.active : '')} end>Home</NavLink></li>
-        <li><NavLink to="/portfolio" className={({ isActive }) => (isActive ? css.active : '')}>Portfolio</NavLink></li>
+        {/* <li><NavLink to="/portfolio" className={({ isActive }) => (isActive ? css.active : '')}>Portfolio</NavLink></li> */}
         <li><NavLink to="/blog" className={({ isActive }) => (isActive ? css.active : '')}>Blog</NavLink></li>
         <li><NavLink to="/contact" className={({ isActive }) => (isActive ? css.active : '')}>Contacts</NavLink></li>
       </ul>
